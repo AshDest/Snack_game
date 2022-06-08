42a2729de963692b2c9b905944f6524566524d77
unknown <ashuzamaheshe4@gmail.com>
2022-06-08 13:18:43 +0200

    other commit

--- snake.js
+++ snake.js
@@ -42,7 +42,18 @@ class Apple{
         var isTouching;
         while (true) {
             isTouching = false;
-            this.x = Math.floor(Math.random())
+            this.x = Math.floor(Math.random() * canvas.width / snake.size) * snake.size
+            this.y = Math.floor(Math.random() * canvas.height / snake.size) * snake.size
+            for (var i = 0; i < snake.tail.length; i++) {
+                if (this.x == snake.tail[i].x && this.y == snake.tail[i].y) {
+                    isTouching = true;
+                }
+            }
+            if (!isTouching) {
+                break;
+            }
+            this.color = "pink"
+            this.size = snake.size
         }
     }
 }
@@ -66,3 +77,37 @@ function show(){
     update();
     draw();
 }
+
+function update(){
+
+}
+function draw(){
+    createRect(0,0,canvas.width, canvas.height, "black")
+    createRect(0,0,canvas.width, canvas.height)
+    for (var i = 0; i < snake.tail.length; i++) {
+        createRect(snake.tail[i].x + 2.5, snake.tail[i].y + 2.5,
+            snake.size - 5, snake.size - 5, 'white')    
+    }
+    canvasContext.font = "20px Arial"
+    canvasContext.fillStyle = "#00FF42"
+    canvasContext.fillText("Score: ", (snake.tail.length + 1), 
+        canvas.width - 120, 18);
+    createRect(apple.x, apple.y, apple.size, apple.size, apple.color)
+}
+
+function createRect(x,y,width,height, color){
+    canvasContext.fillStyle = color
+    canvasContext.fillRect(x,y,width,height)
+
+}
+
+window.addEventListener("keydown", (event)=>{
+    setTimeout(()=>{
+        if (event.keyCode == 37 && snake.rotateX != 1) {
+            snake.rotateX = 1
+            snake.rotateY = 0;
+        } else if (condition) {
+            
+        }
+    })
+})
